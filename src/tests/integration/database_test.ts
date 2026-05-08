4fd30811d754690a94fc44be3ed2038490ee86fe
machafa <penny.machafa@yahoo.com.br>
2026-05-08 19:35:57 +0200

    refactor: wetin dey do to you? I see you

--- src/tests/integration/database_test.ts
+++ src/tests/integration/database_test.ts
@@ -1,4 +1,17 @@
+import { jest } from '@jest/globals';
 import pool from '../../config/database.js';
+
+
+process.env.DATABASE_URL = 'postgres://user:pass@localhost:5432/test_db';
+process.env.NODE_ENV = 'test';
+process.env.API_KEY = 'test_key';
+process.env.MPESA_PUBLIC_KEY = 'test_public_key';
+process.env.MPESA_API_URL = 'https://mock.mpesa.com';
+process.env.MPESA_BASE_URL = 'https://api.sandbox.vm.co.mz:18352/ipg/v1x';
+process.env.MPESA_SERVICE_PROVIDER_CODE ='171717';
+// 2.
+jest.mock('../../modules/payments/payment_repository');
+jest.mock('../../gateways/mpesa_gateway');
 
 describe('Database Integration', () => {
   
