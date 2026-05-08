3fa4654069c1128b8e8042fab265a6d8bf57d3b9
machafa <penny.machafa@yahoo.com.br>
2026-05-08 19:29:37 +0200

    refactor: wetin dey do to youuuuu

--- src/tests/unit/payment_service_test.ts
+++ src/tests/unit/payment_service_test.ts
@@ -6,7 +6,7 @@ process.env.API_KEY = 'test_key';
 process.env.MPESA_PUBLIC_KEY = 'test_public_key';
 process.env.MPESA_API_URL = 'https://mock.mpesa.com';
 process.env.MPESA_BASE_URL = 'https://api.sandbox.vm.co.mz:18352/ipg/v1x';
-
+process.env.MPESA_SERVICE_PROVIDER_CODE ='171717';
 // 2.
 jest.mock('../../modules/payments/payment_repository');
 jest.mock('../../gateways/mpesa_gateway');
